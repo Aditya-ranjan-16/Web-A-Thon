b89f50d672ec5b1da917de9bff105ffe30d22582
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-01-21 17:43:52 +0530

    Update competition.js

--- models/competition.js
+++ models/competition.js
@@ -5,6 +5,15 @@ const competitionSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  des: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  date: { type: Date, default: Date.now },
 });
 
 const competitionList = new mongoose.model("competition", competitionSchema);
