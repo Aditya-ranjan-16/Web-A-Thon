8ccef61c40c8e317029394f082be09ea870433a1
Rishav <rishavrajcrj@gmail.com>
2023-01-21 16:11:02 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user/userController");
+const loginController = require("../controllers/login/loginController");
 const auth = require("../middleWare/auth");
 const { check, validationResult } = require("express-validator");
+
+// Public || Get Register User
+router.post(
+  "/signup",
+  [check("email", "email is Required").not().isEmpty()],
+  [check("email", "email is Required").not().isEmpty()],
+  [check("password", "password is Required").not().isEmpty()],
+  loginController.register
+);
 
 // auth
 router.use(auth);
