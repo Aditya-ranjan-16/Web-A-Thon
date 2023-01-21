794fc569cd6361df481c14fa237cd24d07100281
Rishav <rishavrajcrj@gmail.com>
2023-01-21 16:22:49 +0530

    Update loginController.js

--- controllers/login/loginController.js
+++ controllers/login/loginController.js
@@ -5,6 +5,16 @@ const { check, validationResult } = require("express-validator");
 const nodemailer = require("nodemailer");
 
 const register = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { name, email, password } = req.body;
+
+  console.log(name);
+  console.log(email);
+  console.log(password);
+
   res.status(202).send("Hello");
 };
 
