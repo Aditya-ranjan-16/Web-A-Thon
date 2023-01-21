b254aa4ea14bd5e291b547d83b7a490b3c895c3e
Rishav <rishavrajcrj@gmail.com>
2023-01-21 15:43:18 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const router = express.Router();
-// const userController = require("../controllers/user/userController");
+const userController = require("../controllers/user/userController");
 const auth = require("../middleWare/auth");
 const { check, validationResult } = require("express-validator");
 
+// auth
+router.use(auth);
+
 router.post(
-  "/sendEmail",
+  "/",
   [check("email", "email is Required").not().isEmpty()],
-  [check("message", "message is Required").not().isEmpty()],
-  [check("name", "name is Required").not().isEmpty()],
-  async(req,res)=>{
-    res.status(202).send("Hello send")
-  }
+  userController.getUser
 );
 
 module.exports = router;
