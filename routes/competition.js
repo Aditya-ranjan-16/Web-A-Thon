0ccd272dd8e95b0312ff1930b6f4959d67f986aa
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-01-21 18:02:16 +0530

    Create competition.js

---
+++ routes/competition.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/competition/competition");
+const auth = require("../middleWare/auth");
+const { check, validationResult } = require("express-validator");
+
+// auth
+router.use(auth);
+
+// Public || Get user Data
+router.post(
+  "/",
+  [check("email", "email is Required").not().isEmpty()],
+  userController.getUser
+);
+
+module.exports = router;
