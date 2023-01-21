6530c73eed442ab00822ae4490f7131aa53abfd3
Rishav <rishavrajcrj@gmail.com>
2023-01-21 16:40:11 +0530

    Update loginController.js

--- controllers/login/loginController.js
+++ controllers/login/loginController.js
@@ -3,6 +3,7 @@ const user = require("../../models/userSchema");
 const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
 const nodemailer = require("nodemailer");
+const gravatar = require("gravatar");
 
 const register = async (req, res, next) => {
   const errors = validationResult(req);
@@ -14,6 +15,64 @@ const register = async (req, res, next) => {
   console.log(name);
   console.log(email);
   console.log(password);
+
+  let users;
+  try {
+    users = await user.findOne({ email: email });
+    console.log(users);
+  } catch (e) {
+    const error = new HttpError("Wrong Email Credentials", 400);
+    return next(error);
+  }
+
+  if (users) {
+    res.json({ exists: true });
+    return;
+  } else {
+    let avatar;
+    try {
+      avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
+    } catch (e) {
+      const error = new HttpError("gravatar error", 400);
+      return next(error);
+    }
+
+    const newUser = new user({
+      name: name,
+      email: email,
+      password: password,
+      avatar: avatar,
+    });
+
+    try {
+      const createduser = await newUser.save();
+
+      let token;
+
+      try {
+        token = jwt.sign(
+          { userEmail: email, designation: "user" },
+          process.env.JWT_SECRATE,
+          { expiresIn: "3hr" }
+        );
+      } catch (err) {
+        const error = new HttpError("Error logging user", 401);
+        console.log(err);
+        return next(error);
+      }
+      var userinfo = {
+        name: name,
+        email: email,
+        password: password,
+        avatar: avatar,
+      };
+      res.json({ exists: false, token: token, user: userinfo });
+    } catch (err) {
+      console.log(err);
+      const error = new HttpError("Cannot add user", 400);
+      return next(error);
+    }
+  }
 
   res.status(202).send("Hello");
 };
