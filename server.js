dc04fba3338dfa75622dc15394aa6e87900e718c
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-01-21 18:02:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,35 +11,35 @@ dotenv.config();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, DELETE,OPTIONS"
-        );
-        
-        next();
-    });
-    app.get("/test", (req, res) => {
-        res.send("Hello Server");
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE,OPTIONS"
+  );
+
+  next();
+});
+app.get("/test", (req, res) => {
+  res.send("Hello Server");
 });
 
-app.get("/",(req,res)=>{
-    res.status(202).send("HEllo World")
-})
-
+app.get("/", (req, res) => {
+  res.status(202).send("HEllo World");
+});
 
 app.use("/api/user", require("./routes/user"));
+app.use("/api/competition", require("./routes/competition"));
 
 const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DB_URI).then(
-    app.listen(port, () => {
-        console.log("Listining on port " + port);
-        console.log("connected");
-    })
+  app.listen(port, () => {
+    console.log("Listining on port " + port);
+    console.log("connected");
+  })
 );
