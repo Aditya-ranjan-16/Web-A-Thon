a2bdc426544e976c1bf6772647c51c3dfa8281ef
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-01-22 06:46:47 +0530

    Update request.js

--- routes/request.js
+++ routes/request.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const competition = require("../controllers/request/request");
+const request = require("../controllers/request/request");
 const auth = require("../middleWare/auth");
 const { check, validationResult } = require("express-validator");
 
-router.get("/Add", competition.AllCompetition);
+router.get("/Add", request.AllCompetition);
 
 // auth
 router.use(auth);
