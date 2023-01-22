51e7328a8ec66dc3ceaf06c877158c09a6cdf160
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-01-22 09:51:33 +0530

    Update dashboard.js

--- controllers/Dashboard/dashboard.js
+++ controllers/Dashboard/dashboard.js
@@ -6,21 +6,34 @@ const nodemailer = require("nodemailer");
 
 // Private || All Events of the user
 const getUserEvents = async (req, res, next) => {
-  let userData;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email } = req.body;
+
+  let users;
   try {
-    userData = await user.findOne({ email: res.locals.userData.userEmail });
+    users = await user.findOne({ email });
 
-    if (userData) {
-      console.log(userData._id);
-      //   let competitionData = Competitions.find({ host: userData._id });
-
-      //   res.status(202).send(competitionData);
+    if (!user) {
+      const error = new HttpError("Wrong email", 400);
+      return next(error);
     }
+
+    let competitionData = await Competitions.find({
+      host: users._id,
+    });
+
+    res.status(202).send(competitionData);
   } catch (e) {
-    const error = new HttpError("Email Not Found", 505);
+    const error = new HttpError("Server Error", 505);
     console.log(e);
     return next(error);
   }
 };
+
+// Private
 
 exports.getUserEvents = getUserEvents;
