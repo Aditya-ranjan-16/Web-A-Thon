067ecd3198fb788c72205a7ac75f787177ec5187
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-01-22 07:35:52 +0530

    Update request.js

--- controllers/request/request.js
+++ controllers/request/request.js
@@ -1,5 +1,6 @@
 const HttpError = require("../../models/HttpError");
 const request = require("../../models/requestSchema");
+const Competitions = require("../../models/competitionSchema");
 const { check, validationResult } = require("express-validator");
 const nodemailer = require("nodemailer");
 
@@ -39,9 +40,20 @@ const statusCheck = async (req, res, next) => {
 
   const { competitionID, userID } = req.body;
 
-  res
-    .status(202)
-    .send({ message: `competitionID=${competitionID}, userID=${userID}` });
+  try {
+    const requestData = await request.findOne({ competitionID, userID });
+
+    if (!requestData) {
+      const error = new HttpError("Wrong competitionID", 400);
+      return next(error);
+    }
+
+    return res.status(202).send(requestData);
+  } catch (e) {
+    console.log(e);
+    const error = new HttpError("Wrong Email Credentials", 400);
+    return next(error);
+  }
 };
 
 exports.addReq = addReq;
