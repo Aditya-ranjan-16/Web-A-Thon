4a9b516274d16dafebf0cdca48e7e58011679f67
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-01-22 07:16:01 +0530

    Update request.js

--- routes/request.js
+++ routes/request.js
@@ -4,6 +4,9 @@ const request = require("../controllers/request/request");
 const auth = require("../middleWare/auth");
 const { check, validationResult } = require("express-validator");
 
+// auth
+// router.use(auth);
+
 // Private || Add Request to join a event
 router.post(
   "/Add",
@@ -14,7 +17,12 @@ router.post(
   request.addReq
 );
 
-// auth
-// router.use(auth);
+// Private || Request Status Check
+router.post(
+  "/Status",
+  [check("competitionID", "competitionID is Required").not().isEmpty()],
+  [check("userID", "userID is Required").not().isEmpty()],
+  request.statusCheck
+);
 
 module.exports = router;
