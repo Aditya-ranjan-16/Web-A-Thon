53a6fa04d09debff6682b48c77ae67a2baa7f1fb
Rishav Raj Singh <rishavrajcrj@gmail.com>
2023-01-22 08:12:28 +0530

    Create dashborads.js

---
+++ routes/dashborads.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const request = require("../controllers/request/request");
+const auth = require("../middleWare/auth");
+const { check, validationResult } = require("express-validator");
+
+// auth
+// router.use(auth);
+
+// Private || Add Request to join a event
+router.post(
+  "/Add",
+  [check("competitionID", "competitionID is Required").not().isEmpty()],
+  [check("userID", "userID is Required").not().isEmpty()],
+  [check("hostID", "hostID is Required").not().isEmpty()],
+  [check("message", "message is Required").not().isEmpty()],
+  request.addReq
+);
+
+module.exports = router;
